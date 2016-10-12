6b033eb770441c2465a0fc69de6e0c85bccac1c9
Matthias Simon <simon@infonautika.de>
2016-10-12 09:02:19 +0200

    template link creation

--- src/main/java/de/infonautika/monomusiccorp/app/controller/InfoController.java
+++ src/main/java/de/infonautika/monomusiccorp/app/controller/InfoController.java
@@ -3,12 +3,13 @@ package de.infonautika.monomusiccorp.app.controller;
 import de.infonautika.monomusiccorp.app.controller.resources.MessageResource;
 import de.infonautika.monomusiccorp.app.controller.utils.AuthorizedLinkBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.TemplateVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import static de.infonautika.monomusiccorp.app.controller.utils.LinkSupport.addLink;
+import static de.infonautika.monomusiccorp.app.controller.utils.LinkSupport.addTemplateLink;
+import static de.infonautika.monomusiccorp.app.controller.utils.LinkSupport.pathTemplateVariable;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
@@ -39,19 +40,10 @@ public class InfoController {
                 addLink(messageResource, "stock")
         );
 
-//        authorizedLinkBuilder.withRightsOn(
-//                methodOn(StockController.class).getStockItem("id"),
-//                invocationAware -> {
-//                    TemplateVariables variables = new TemplateVariables(pathTemplateVariable("id", "helloo"));
-//                    Link link = new Link(new UriTemplate("/api/stock/{id}", variables), "stockitem");
-//                    messageResource.add(link);
-//                }
-//
-//        );
-    }
-
-    private TemplateVariable pathTemplateVariable(String name, String description) {
-        return new TemplateVariable(name, TemplateVariable.VariableType.PATH_VARIABLE, description);
+        authorizedLinkBuilder.withRightsOn(
+                methodOn(StockController.class).getStockItem("id"),
+                addTemplateLink(messageResource, "stockitem", pathTemplateVariable("id"))
+        );
     }
 
     private void addCustomerControllerLinks(MessageResource messageResource) {
--- src/main/java/de/infonautika/monomusiccorp/app/controller/utils/LinkSupport.java
+++ src/main/java/de/infonautika/monomusiccorp/app/controller/utils/LinkSupport.java
@@ -1,7 +1,10 @@
 package de.infonautika.monomusiccorp.app.controller.utils;
 
-import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.*;
+import org.springframework.hateoas.core.AnnotationMappingDiscoverer;
 import org.springframework.hateoas.core.DummyInvocationUtils;
+import org.springframework.hateoas.core.MappingDiscoverer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.function.Consumer;
 
@@ -9,8 +12,31 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 public class LinkSupport {
 
+    private static final MappingDiscoverer MAPPING_DISCOVERER = new AnnotationMappingDiscoverer(RequestMapping.class);
+
     public static Consumer<DummyInvocationUtils.LastInvocationAware> addLink(ResourceSupport resource, String relationName) {
         return invocationAware -> resource.add(linkTo(invocationAware).withRel(relationName));
     }
 
+    public static Consumer<DummyInvocationUtils.LastInvocationAware> addTemplateLink(ResourceSupport resource, String relationName, TemplateVariable... templateVariables) {
+        TemplateVariables variables = new TemplateVariables(templateVariables);
+
+        // Somehow, ControllerLinkBuilder does not give the option to add TemplateVariables to build a
+        // template link, does it?
+        // Maybe this will be provided in future versions
+        return invocationAware -> resource.add(
+            new Link(
+                new UriTemplate(
+                        MAPPING_DISCOVERER.getMapping(invocationAware.getLastInvocation().getMethod()),
+                        variables),
+                relationName));
+    }
+
+    public static TemplateVariable pathTemplateVariable(String name) {
+        return new TemplateVariable(name, TemplateVariable.VariableType.PATH_VARIABLE, "");
+    }
+
+    public static TemplateVariable pathTemplateVariable(String name, String description) {
+        return new TemplateVariable(name, TemplateVariable.VariableType.PATH_VARIABLE, description);
+    }
 }
