33e16ae93cb61346019529f9572d4eb8df5dab2e
Matthias Simon <simon@infonautika.de>
2016-10-16 07:13:00 +0200

    no inheritance for now

--- src/main/java/de/infonautika/monomusiccorp/app/controller/resources/PricedPositionResourceAssembler.java
+++ src/main/java/de/infonautika/monomusiccorp/app/controller/resources/PricedPositionResourceAssembler.java
@@ -1,6 +1,5 @@
 package de.infonautika.monomusiccorp.app.controller.resources;
 
-import de.infonautika.monomusiccorp.app.domain.Position;
 import de.infonautika.monomusiccorp.app.domain.PricedPosition;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 
@@ -14,14 +13,9 @@ public class PricedPositionResourceAssembler extends ResourceAssemblerSupport<Pr
 
     @Override
     public PricedPositionResource toResource(PricedPosition pricedPosition) {
-
-        // TODO: 14.10.16 make PricedPosition extend Position and PricedPositionResourceAssembler extend PositionResourceAssembler
-        Position position = new Position(pricedPosition.getProduct(), pricedPosition.getQuantity());
-        PositionResource positionResource = new PositionResourceAssembler(controllerClass).toResource(position);
-
         PricedPositionResource pricedPositionResource = new PricedPositionResource();
-        pricedPositionResource.setProductId(positionResource.getProductId());
-        pricedPositionResource.setQuantity(positionResource.getQuantity());
+        pricedPositionResource.setProductId(pricedPosition.getProduct().getId());
+        pricedPositionResource.setQuantity(pricedPosition.getQuantity());
         pricedPositionResource.setPrice(pricedPosition.getPrice());
         return pricedPositionResource;
     }
